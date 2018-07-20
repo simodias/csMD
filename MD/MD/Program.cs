1c005a2820c45d3c5a977a1718cbf2559ee570cc
simodias <szymon.mackowiak@gmail.com>
2018-07-20 16:48:38 +0200

    Initial commmit.

---
+++ MD/MD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MatPoint
+{
+    double x, y, z;
+    double xOld, yOld, zOld;
+
+    double vx, vy, vz;
+    double vxOld, vyOld, vzOld;
+
+    double ax, ay, az;
+    double axOld, ayOld, azOld;
+
+    double fx, fy, fz;
+    double fxOld, fyOld, fzOld;
+
+};
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("THE END");
+            System.Console.ReadLine();
+        }
+    }
+}
+
